9a7d3bfab4a13cdbc2234f58b0a15f5c3839397b
Daniel Choiniere <danielchoiniere.student@careerdevs.com>
2018-11-26 03:57:25 +0000

    mutations algorithm

--- mutations.js
+++ mutations.js
@@ -1,12 +1,13 @@
-function chunkArrayInGroups(arr, size) {
-  // Break it up.
-  var newArr = [];
-
-  for (var i=0; i<arr.length; i++) {
-    console.log(arr.slice(0, size));
-  }
- 
-  console.log(arr);
+function mutation(arr) {
+  var lowercaseArrOne = arr[0].toLowerCase();
+  var lowercaseArrTwo = arr[1].toLowerCase();
+  // console.log(lowercaseArrOne);
+  // console.log(lowercaseArrTwo);
+  for (var i=0;  i < lowercaseArrTwo.length; i++) {
+    if (lowercaseArrOne.indexOf(lowercaseArrTwo[i]) != -1 )
+      return true;
+    }
+  return false;
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+console.log(mutation(["hello", "hey"]));
